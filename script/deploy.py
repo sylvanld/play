713c860d51f74c593bc2bfe52f77272ab4f3608b
Sylvan LE DEUNFF <sledeunf@gmail.com>
2020-01-29 11:26:43 +0100

    tmp

--- script/deploy.py
+++ script/deploy.py
@@ -1,4 +1,25 @@
 import json
+import subprocess
+
+def current_branch():
+    output = subprocess.check_output('git branch', shell=True)
+    matches = filter(lambda s: s.startswith('*'), output.decode('utf-8').strip().split('\n'))
+    branch = tuple(matches)[0][2:]
+    return branch
+    """
+    [
+        current_branch,
+        'git checkout dev',
+        'increment version',
+        'git add .',
+        'git commit -m TRUC: version note',
+        'git push origin dev',
+        'git checkout master',
+        'git pull origin dev',
+        'git tag VERSION'
+        'git push --tags' <-- deployed
+    ]
+    """
 
 def cinput(*messages, required=False, choices=None):
     """
@@ -25,21 +46,28 @@ def cinput(*messages, required=False, choices=None):
     print('-'*60)
     return response
 
-# input type of version
-version_types = ['patch', 'minor', 'major']
-type_index = cinput('Select version type', choices=version_types)
+def increment_version():
+    version_types = ['patch', 'minor', 'major']
+    type_index = cinput('Select version type', choices=version_types)
 
-# increment (in-memory) current version
-package_data = json.load(open('package.json'))
-version = [int(x) for x in package_data['version'].split('.')]
+    # increment (in-memory) current version
+    package_data = json.load(open('package.json'))
+    version = [int(x) for x in package_data['version'].split('.')]
 
-version = [version[i]*(i+type_index<=2) + 1*(2==i+type_index) for i in range(3)]
-version = '.'.join([str(x) for x in version])
+    version = [version[i]*(i+type_index<=2) + 1*(2==i+type_index) for i in range(3)]
+    version = '.'.join([str(x) for x in version])
 
-# ask message
-version_note = cinput("Note for version (%s)"%version)
-commit_message = version_types[type_index].upper() + ': ' + version_note
+    # ask message
+    version_note = cinput("Note for version (%s)"%version)
+    commit_message = version_types[type_index].upper() + ': ' + version_note
 
-# save version note and commit
-package_data['version'] = version
-json.dump(package_data, open('package.json', 'w'))
+    # save version note and commit
+    package_data['version'] = version
+    json.dump(package_data, open('package.json', 'w'))
+
+
+old_branch = current_branch()
+
+subprocess.call('git checkout dev', shell=True)
+
+print(old_branch, current_branch())
