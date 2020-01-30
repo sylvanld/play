127df56a49b36decd1a502e5774968080a89c69f
Sylvan LE DEUNFF <sledeunf@gmail.com>
2020-01-30 18:16:13 +0100

    fix subprocess commands syntax

--- play/play/features.py
+++ play/play/features.py
@@ -3,12 +3,12 @@ import subprocess
 from .common import current_branch, abort, sanitize
 
 def create_feature(name):
-    feature_name = 'feature/' + sanitize('name')
+    feature_name = 'feature/' + sanitize(name)
     commands = (
-        ['git', 'checkout', 'dev'],
-        ['git', 'pull'],
-        ['git', 'checkout', '-b', feature_name],
-        ['git', 'push', '-u', 'origin', feature_name]
+        'git checkout dev',
+        'git pull',
+        'git checkout -b %s'%feature_name,
+        'git push -u origin %s'%feature_name
     )
     print(commands)
     for command in commands:
@@ -21,13 +21,13 @@ def merge_current_feature():
         abort('you can only close a feature branch')
     
     commands = (
-        ['git', 'add', '.'],
-        ['git', 'commit', '-m', '"work done for branch %s"'%feature_branch],
-        ['git', 'push', 'origin', feature_branch],
-        ['git', 'checkout', 'dev'],
-        ['git', 'pull']
-        ['git', 'merge', feature_branch]
-        ['git', 'push']
+        'git add .',
+        'git commit -m "work done for branch %s"'%feature_branch,
+        'git push origin %s'%feature_branch,
+        'git checkout dev',
+        'git pull',
+        'git merge %s'%feature_branch,
+        'git push'
     )
 
     for command in commands:
