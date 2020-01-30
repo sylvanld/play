cf5052e5a475caac83ee8b47cff9c272fc9037aa
Sylvan LE DEUNFF <sledeunf@gmail.com>
2020-01-30 11:22:55 +0100

    add deployment script

--- script/__main__.py
+++ script/__main__.py
@@ -6,7 +6,6 @@ def parse_args():
 
     deploy_parser = subparsers.add_parser('deploy')
     deploy_parser.set_defaults(command='deploy')
-    deploy_parser.add_argument('version_note', nargs='*', type=str)
     deploy_parser.add_argument('--patch', action='store_const', default=0, const=0)
     deploy_parser.add_argument('--minor', action='store_const', default=0, const=1)
     deploy_parser.add_argument('--major', action='store_const', default=0, const=2)
@@ -20,12 +19,11 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print(args)
 
     if args.command == 'deploy':
         from .deploy import deploy
         version_index = args.major or args.minor or args.patch
-        deploy(version_index, version_note=" ".join(args.version_note))
+        deploy(version_index)
 
 if __name__ == '__main__':
     main()
