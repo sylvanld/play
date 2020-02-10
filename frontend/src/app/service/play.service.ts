0dcab4e5d6b577745e6fbb6ffb5ce2f6bdce72d4
Sylvan LE DEUNFF <sledeunf@gmail.com>
2020-02-10 15:07:29 +0100

    initiate play service

--- frontend/src/app/service/play.service.ts
+++ frontend/src/app/service/play.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ProviderService } from './provider.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PlayService {
+export class PlayService extends ProviderService {
 
-  constructor() { }
+  constructor(
+    protected http: HttpClient,
+    private auth: AuthenticationService
+  ) {
+    super(http, environment.play_api_url, { type: 'bearer' });
+  }
+
+  getToken(): Observable<string> {
+    return
+  }
 }
