0186d314eb1c25493ff31665aac54166ebf014fa
Sylvan LE DEUNFF <sledeunf@gmail.com>
2020-02-17 02:46:12 +0100

    add password strength hit, fix replay request in provider

--- frontend/src/app/component/auth/edit-password/edit-password.component.ts
+++ frontend/src/app/component/auth/edit-password/edit-password.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NotificationService } from 'src/app/service/notification.service';
 
+interface PasswordCheck {
+  function: () => number;
+  weight: number;
+  hint: string;
+  required: boolean;
+}
+
 @Component({
   selector: 'app-edit-password',
   templateUrl: './edit-password.component.html',
@@ -9,6 +16,35 @@ import { NotificationService } from 'src/app/service/notification.service';
 export class EditPasswordComponent implements OnInit {
   @Input() passwordExist = false;
   editing = false;
+  password: string = "";
+  hint: string = "";
+  pwdChecks: PasswordCheck[] = [
+    {
+      required: true,
+      hint: 'Password length must be at least 6 characters (should be > 8)',
+      weight: 2,
+      function: () => this.password.length > 7 ? 2 : (this.password.length > 5 ? 1 : 0)
+    },
+    {
+      required: false,
+      hint: 'Password should contains a capital letter',
+      weight: 1,
+      function: () => /[A-Z]/.test(this.password) ? 1 : 0
+    },
+    {
+      required: false,
+      hint: 'Password should contains a number',
+      weight: 1,
+      function: () => /[0-9]/.test(this.password) ? 1 : 0
+    },
+    {
+      required: false,
+      hint: 'Password should contains a special character',
+      weight: 1,
+      function: () => /[^A-Za-z0-9]/.test(this.password) ? 1 : 0
+    }
+  ];
+
 
   constructor(private notify: NotificationService) { }
 
@@ -24,5 +60,25 @@ export class EditPasswordComponent implements OnInit {
     this.notify.info('Password has been changed!');
     this.editing = false;
     this.passwordExist = true;
+  }
+
+  get passwordStrength() {
+    let score = 0;
+    let maxi = 0
+    let result;
+
+    this.hint = "";
+    for (const check of this.pwdChecks) {
+      result = check.function();
+      if (result === 0) {
+        this.hint = check.hint;
+        if (check.required) return 0;
+      };
+
+      score += result;
+      maxi += check.weight;
+    }
+
+    return 100 * score / maxi;
   }
 }
